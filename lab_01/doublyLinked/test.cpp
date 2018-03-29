93d7c82c7c225003489a97e29a2507a5e9cdfb2e
mdapathy <md.apathy@gmail.com>
2018-03-29 19:45:55 +0300

    Update test.cpp

--- lab_01/doublyLinked/test.cpp
+++ lab_01/doublyLinked/test.cpp
@@ -7,23 +7,23 @@ using namespace std;
 TEST_CASE("Properties of a new list: size, head, tail")
 {
 	Doubly_linked list;
-
+	
 	CHECK(list.get_size() == 0);
-
+	
 	CHECK_THROWS(*list.begin()); //list's empty
 	CHECK_THROWS(*list.end());
 }
 
 TEST_CASE("Push in  empty and nonempty list") // check whether the properties are updated
 { 
-    Doubly_linked list;
+        Doubly_linked list;
 
 	REQUIRE_NOTHROW(list.push(6));
-    CHECK(*list.begin() == 6);
+        CHECK(*list.begin() == 6);
 	CHECK(*list.end() == 6);
 
 	REQUIRE_NOTHROW(list.push(7));
-    CHECK(*list.begin() == 6 );
+        CHECK(*list.begin() == 6 );
 	CHECK(*list.end() == 7 );
 
 }
@@ -44,13 +44,13 @@ TEST_SUITE_BEGIN("Removing");
 
 TEST_CASE("Removing exceptions ") 
 {    
-    Doubly_linked list;
+        Doubly_linked list;
 
-    CHECK_THROWS(list.remove(0)); // list is  empty 
+        CHECK_THROWS(list.remove(0)); // list is  empty 
 	CHECK_THROWS(list.remove(-30)); 
      
-    list.push(5);
-    list.push(4);
+        list.push(5);
+        list.push(4);
 
 	CHECK_THROWS(list.remove(1000) ); // exceeds list's size
 
@@ -58,13 +58,13 @@ TEST_CASE("Removing exceptions ")
 
 TEST_CASE("Removing from list with one obj") // updating head and tail 
 {
-    Doubly_linked list;
-    list.push(45);
+        Doubly_linked list;
+        list.push(45);
 
 
-    REQUIRE_NOTHROW(list.remove(0));
+        REQUIRE_NOTHROW(list.remove(0));
 
-    REQUIRE_THROWS(*list.begin());
+        REQUIRE_THROWS(*list.begin());
 	REQUIRE_THROWS(*list.end()); 
 	CHECK(list.get_size() == 0);
 
@@ -73,14 +73,14 @@ TEST_CASE("Removing from list with one obj") // updating head and tail
 
 TEST_CASE("Removing tail")
 {   
-	 Doubly_linked list;
+	Doubly_linked list;
 
-     list.push(40); // filling the list
-     list.push(59); 
+	list.push(40); // filling the list
+	list.push(59); 
 
-     REQUIRE_NOTHROW(list.remove(1));
+	REQUIRE_NOTHROW(list.remove(1));
 
-     CHECK(*list.end() == 40);
+	CHECK(*list.end() == 40);
 
 }
 
@@ -114,9 +114,9 @@ TEST_CASE("Inserting in  empty list")
 {
 	Doubly_linked list2;
 
-    CHECK(list2.get_size() == 0);
+	CHECK(list2.get_size() == 0);
 	REQUIRE_NOTHROW(list2.insert(1, 0));
-    CHECK(list2.get_size() == 1);
+	CHECK(list2.get_size() == 1);
 	CHECK(*list2.begin() == 1 );
 	CHECK(*list2.end() == 1);
 
@@ -126,9 +126,9 @@ TEST_CASE("Inserting in  empty list")
 
 TEST_CASE("Inserting head")
 {
-   Doubly_linked list2;
-   REQUIRE_NOTHROW(list2.insert(0, 0));
-   CHECK(*list2.begin() == 0 );
+	Doubly_linked list2;
+   	REQUIRE_NOTHROW(list2.insert(0, 0));
+   	CHECK(*list2.begin() == 0 );
 
 };
 
@@ -148,9 +148,9 @@ TEST_SUITE_END();
 TEST_CASE("Turning list into string") // throwing exception if the list is empty 
 {
 	Doubly_linked list1, list2;
-    list2.push(0);
-    list2.push(1);
-    list2.push(2);
+    	list2.push(0);
+    	list2.push(1);
+    	list2.push(2);
 
 	REQUIRE_THROWS(list1.to_string());
 	CHECK(list2.to_string().compare("0 1 2") == 0); //returns 0 if the strings are equal
@@ -166,7 +166,7 @@ TEST_CASE("Delete every third: empty list")
 
 TEST_CASE("Delete every third including tail") 
 {
-    Doubly_linked list3;
+    	Doubly_linked list3;
 
 	REQUIRE_NOTHROW(list3.push(1));
 	REQUIRE_NOTHROW(list3.push(2));
@@ -174,9 +174,9 @@ TEST_CASE("Delete every third including tail")
 	
 	CHECK(list3.get_size() == 3);
 
-    REQUIRE_NOTHROW(list3.delete_every_third());
+   	REQUIRE_NOTHROW(list3.delete_every_third());
 
-    CHECK(*list3.end() == 2);
+   	CHECK(*list3.end() == 2);
 
 };
 
@@ -203,51 +203,51 @@ TEST_CASE("Increment operator")
 
 	Doubly_linked::List_Iterator iter = list3.begin();
 
-    CHECK(*iter == 20);
+    	CHECK(*iter == 20);
 	REQUIRE_NOTHROW(++iter);
-    CHECK(*iter == 15);
+    	CHECK(*iter == 15);
 };
 
 TEST_CASE("Increment operator exceptions")
 {
-   Doubly_linked list;
-   list.push(1);
-   Doubly_linked::List_Iterator iter = list.begin();
-   CHECK(*iter == 1);
+   	Doubly_linked list;
+   	list.push(1);
+   	Doubly_linked::List_Iterator iter = list.begin();
+   	CHECK(*iter == 1);
   
-   REQUIRE_THROWS(++iter);
+  	REQUIRE_THROWS(++iter);
 };
 
 TEST_CASE("Decrement operator")
 {
-   Doubly_linked list;
-   list.push(14);
-   list.push(19);
+	Doubly_linked list;
+   	list.push(14);
+   	list.push(19);
 
-   Doubly_linked::List_Iterator iter = list.end();
-   CHECK(*iter == 19);
+   	Doubly_linked::List_Iterator iter = list.end();
+   	CHECK(*iter == 19);
 
-   REQUIRE_NOTHROW(--iter);
-   CHECK(*iter == 14);
+   	REQUIRE_NOTHROW(--iter);
+   	CHECK(*iter == 14);
 };
 
 TEST_CASE("Decrement operator exceptions")
 {
-  Doubly_linked list;
-  list.push(20);
+  	Doubly_linked list;
+  	list.push(20);
 
-  Doubly_linked::List_Iterator iter = list.end();
+  	Doubly_linked::List_Iterator iter = list.end();
 
-  CHECK(*iter == 20);
-  REQUIRE_THROWS(--iter);
+  	CHECK(*iter == 20);
+  	REQUIRE_THROWS(--iter);
 };
 
 TEST_CASE("* pointer exception") 
 {
-   Doubly_linked list;
-   Doubly_linked::List_Iterator iter = list.end();
+	Doubly_linked list;
+   	Doubly_linked::List_Iterator iter = list.end();
 
-   REQUIRE_THROWS(*iter); 
+  	REQUIRE_THROWS(*iter); 
 
 };
 
